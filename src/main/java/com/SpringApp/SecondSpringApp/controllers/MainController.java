fd818b31c8ab560a769bd5c517b01ce1b578531e
Max <cvb.cvb.2017.cvb@gmail.com>
2023-01-23 21:06:06 +0300

    create web page

---
+++ src/main/java/com/SpringApp/SecondSpringApp/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.SpringApp.SecondSpringApp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String hello(Model model){
+        model.addAttribute("title","Main page");
+        return "home";
+    }
+}
