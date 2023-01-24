7d3c63e35393440ace4dc77346c5b202a4e87cae
Max <cvb.cvb.2017.cvb@gmail.com>
2023-01-24 21:23:08 +0300

    I found my mistake)))))))AOAOAOAOAOAO

--- src/main/java/com/SpringApp/SecondSpringApp/models/Users.java
+++ src/main/java/com/SpringApp/SecondSpringApp/models/Users.java
@@ -1,21 +1,17 @@
 package com.SpringApp.SecondSpringApp.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Generated;
-import org.hibernate.annotations.Table;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-
+@Table(name = "users")
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/SpringApp/SecondSpringApp/repo/UsersRepository.java
+++ src/main/java/com/SpringApp/SecondSpringApp/repo/UsersRepository.java
@@ -2,6 +2,8 @@ package com.SpringApp.SecondSpringApp.repo;
 
 import com.SpringApp.SecondSpringApp.models.Users;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepository extends CrudRepository<Users,Long> {
 }
