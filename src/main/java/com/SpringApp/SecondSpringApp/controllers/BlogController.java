a4444d5510834bbd818f0c8865df5c1cca97ce74
Max <cvb.cvb.2017.cvb@gmail.com>
2023-01-25 21:31:07 +0300

    data from site successfully added to database

--- src/main/java/com/SpringApp/SecondSpringApp/controllers/BlogController.java
+++ src/main/java/com/SpringApp/SecondSpringApp/controllers/BlogController.java
@@ -7,11 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
-public class SecondController {
+public class BlogController {
 
     @Autowired
     private UsersRepository usersRepository;
@@ -20,7 +25,7 @@ public class SecondController {
     public String blog(Model model){
         Iterable<Users> users = usersRepository.findAll();
         model.addAttribute("users",users);
-        return "secondPage";
+        return "blogController";
     }
     @GetMapping("/blog/add")
     public String blogAdd(Model model){
@@ -32,5 +37,15 @@ public class SecondController {
         Users user = new Users(title,anons,full_text);
         usersRepository.save(user);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") Long id, Model model){
+        if(!usersRepository.existsById(id)) return "redirect:/blog";
+       Optional<Users> user = usersRepository.findById(id);
+        ArrayList<Users> res = new ArrayList<>();
+        user.ifPresent(res::add);
+       model.addAttribute("state",res);
+       return "blog-details";
     }
 }
